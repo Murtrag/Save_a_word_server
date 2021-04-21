e3796044eee32442693a637ffb7ed36e73c47ae7
murtrag <lukasz.czernik@gmail.com>
2021-04-21 14:47:35 +0200

    Revert "Added dotenv support #5"
    This reverts commit 1fe32406a302ea8c1bf46dffc806a44e21436bf5.

--- save_a_word/save_a_word/wsgi.py
+++ save_a_word/save_a_word/wsgi.py
@@ -9,14 +9,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-try:
-	from dotenv import load_dotenv
-
-	project_folder = os.path.expanduser('~/SaveAWord/Save_a_word_server')  # adjust as appropriate
-	load_dotenv(os.path.join(project_folder, '.env'))
-except Exception as e:
-	print(e)
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'save_a_word.settings')
