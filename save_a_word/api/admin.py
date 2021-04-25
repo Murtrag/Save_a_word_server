7178af3a77fcdf7966ce44a4c799852e11cd7321
murtrag <lukasz.czernik@gmail.com>
2021-04-25 16:40:27 +0200

    Renamed fields in db

--- save_a_word/api/admin.py
+++ save_a_word/api/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from api import models
 
-@admin.register(models.Word)
-class LanguageAdmin(admin.ModelAdmin):
-	list_display  = ('pk','owner', 'language', 'word',)
 
 @admin.register(models.Language)
 class LanguageAdmin(admin.ModelAdmin):
 	list_display  = ('pk', 'code', 'name',)
+
+@admin.register(models.Word)
+class WordAdmin(admin.ModelAdmin):
+	list_display  = ('pk','owner', 'base_word', 'translated_word')
--- save_a_word/api/models.py
+++ save_a_word/api/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 import jsonfield
 
 class Language(models.Model):
-	code = models.CharField(max_length=15)
-	name = models.CharField(max_length=50)
+	code = models.CharField(max_length=15) # unique
+	name = models.CharField(max_length=50) # unique
 
 	def __str__(self):
 		return self.code
@@ -12,9 +12,13 @@ class Language(models.Model):
 
 class Word(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	language = models.ForeignKey(Language, on_delete=models.CASCADE)
-	word = models.TextField()
-	translation = jsonfield.JSONField()
+
+	base_word = models.TextField()
+	base_language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name="base_language")
+
+	translated_word = jsonfield.JSONField()
+	foreign_language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name="foreign_language")
+
 
 	def __str__(self):
-		return f'{self.word}'
+		return f'{self.base_word}'
