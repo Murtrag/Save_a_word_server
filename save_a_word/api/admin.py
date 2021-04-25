397839e8ae43f60fb2099e4cec343139bcee02bf
murtrag <lukasz.czernik@gmail.com>
2021-04-25 16:46:53 +0200

    User registration creates a token #12

--- save_a_word/api/admin.py
+++ save_a_word/api/admin.py
@@ -1,5 +1,9 @@
-from django.contrib import admin
 from api import models
+from django.contrib import admin
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 
 @admin.register(models.Language)
@@ -9,3 +13,9 @@ class LanguageAdmin(admin.ModelAdmin):
 @admin.register(models.Word)
 class WordAdmin(admin.ModelAdmin):
 	list_display  = ('pk','owner', 'base_word', 'translated_word')
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
